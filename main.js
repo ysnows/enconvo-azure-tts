b60306aa8a4629bcd45c4e3cb0b1d0ded1d1767f
guangjin2.xian <guangjin2.xian@zhaopin.com.cn>
2023-05-21 14:08:52 +0800

    feat: 1. v0.0.9

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ function main(text, contextText, completion) {
                                 "ssml": `<speak xmlns=\"http://www.w3.org/2001/10/synthesis\" xmlns:mstts=\"http://www.w3.org/2001/mstts\" version=\"1.0\" xml:lang=\"${target_lang}\"><voice name=\"${target_voice}\"><mstts:express-as><prosody rate=\"1\" pitch=\"0%\">${translate_text}</prosody></mstts:express-as></voice></speak>`
                             }
                         });
-                    completion(undefined)
+                    completion(contextText)
                 } catch (e) {
                     throw e;
                 }
